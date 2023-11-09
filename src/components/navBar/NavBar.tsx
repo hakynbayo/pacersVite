aacc61554e85253cb092f3df0afde8b99f559dcf
hakynbayo <akinbayo.akinwande@decagon.dev>
2023-11-09 09:34:48 +0100

    home

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -21,7 +21,9 @@ const NavBar = ({ isHome }: { isHome?: boolean }) => {
       <>
         <div className="mb-6 flex w-full items-center justify-between px-6">
           <div className="relative h-[34px] w-[80px]">
+          <Link to="/Dashboard">
             <h4>LOGO</h4>
+            </Link>
           </div>
           <div className="relative h-[24px] w-[24px]">
             <img
@@ -34,7 +36,9 @@ const NavBar = ({ isHome }: { isHome?: boolean }) => {
         </div>
         <GenModal isOpen={open} handleCloseModal={toggleModal}>
           <div className="relative h-[56px] w-[131px]">
+          <Link to="/Dashboard">
             <h4>LOGO</h4>
+            </Link>
           </div>
           <div className="item-center mt-10 gap-10">
             {navbarItems.map((item, index) => (
